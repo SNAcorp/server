ce58f04be6dffcf0d17425f94d5ccd124f5c259f
SNA <snagithub@gmail.com>
2024-07-17 00:01:49 +0800

    repair db

--- app/database.py
+++ app/database.py
@@ -3,15 +3,10 @@ from sqlalchemy.orm import sessionmaker
 from app.models import TerminalState
 import os
 
-DATABASE_URL = 'postgresql+asyncpg://nikitastepanov@localhost/terminals'
+DATABASE_URL = os.getenv('DATABASE_URL')
 # os.getenv('DATABASE_URL',
 engine = create_async_engine(DATABASE_URL, echo=True)  # Создание асинхронного движка базы данных
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
-
-
-
-
-
 async def get_db():
     async with SessionLocal() as session:
         yield session  # Создание асинхронной сессии для взаимодействия с базой данных
