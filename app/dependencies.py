bec995ebbd674b38a86ad2ed9a81ed5fa3f912a3
SNA <snagithub@gmail.com>
2024-07-29 16:18:03 +0800

    repair admin panel functions

--- app/dependencies.py
+++ app/dependencies.py
@@ -44,7 +44,7 @@ async def get_current_user(request: Request, db: AsyncSession = Depends(get_db))
 
 async def get_admin_user(current_user: User = Depends(get_current_user)):
     if current_user.is_superuser:
-        raise HTTPException(status_code=403, detail="Not enough permissions")
+        raise HTTPException(status_code=403, detail="Not enough permissions1")
     return current_user
 
 
