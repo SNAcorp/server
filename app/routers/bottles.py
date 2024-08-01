64f6fad4f3f1c13e59ccc19c6dc640525ea92d78
SNA <snagithub@gmail.com>
2024-08-01 11:37:21 +0800

    repair upload images

--- app/routers/bottles.py
+++ app/routers/bottles.py
@@ -120,8 +120,8 @@ async def update_bottle(
         winery: str = Form(...),
         rating_average: float = Form(...),
         location: str = Form(...),
-        image_path300: str = Form(...),
-        image_path600: str = Form(...),
+        image_path300_hidden: str = Form(...),
+        image_path600_hidden: str = Form(...),
         description: str = Form(...),
         wine_type: str = Form(...),
         volume: float = Form(...),
@@ -142,8 +142,8 @@ async def update_bottle(
         bottle.winery = winery
         bottle.rating_average = rating_average
         bottle.location = location
-        bottle.image_path300 = image_path300
-        bottle.image_path600 = image_path600
+        bottle.image_path300 = image_path300_hidden
+        bottle.image_path600 = image_path600_hidden
         bottle.description = description
         bottle.wine_type = wine_type
         bottle.volume = volume
