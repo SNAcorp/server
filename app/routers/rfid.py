20755ce8d922f1675fc5faae2e1880d23ab33a2a
SNA <snagithub@gmail.com>
2024-08-02 18:18:43 +0800

    repair bottle usage

--- app/routers/rfid.py
+++ app/routers/rfid.py
@@ -17,20 +17,20 @@ async def validate_rfid(rfid_code: str, db: AsyncSession = Depends(get_db)):
     if rfid is None:
         return JSONResponse(content={"is_valid": False})
 
-    if rfid.is_valid:
-        return JSONResponse(content={"is_valid": True})
+    # if rfid.is_valid:
+    return JSONResponse(content={"is_valid": True})
 
-    if rfid.limit and rfid.last_used:
-        elapsed_time = datetime.utcnow() - rfid.last_used
-        remaining_time = timedelta(minutes=10) - elapsed_time
-        if remaining_time.total_seconds() > 0:
-            return JSONResponse(content={"is_valid": False, "limit": str(remaining_time)})
-        else:
-            # Сброс ограничений по истечению 10 минут
-            rfid.usage_count = 0
-            rfid.is_valid = True
-            rfid.limit = False
-            await db.commit()
-            return JSONResponse(content={"is_valid": True})
-
-    return JSONResponse(content={"is_valid": False})
+    # if rfid.limit and rfid.last_used:
+    #     elapsed_time = datetime.utcnow() - rfid.last_used
+    #     remaining_time = timedelta(minutes=10) - elapsed_time
+    #     if remaining_time.total_seconds() > 0:
+    #         return JSONResponse(content={"is_valid": False, "limit": str(remaining_time)})
+    #     else:
+    #         # Сброс ограничений по истечению 10 минут
+    #         rfid.usage_count = 0
+    #         rfid.is_valid = True
+    #         rfid.limit = False
+    #         await db.commit()
+    #         return JSONResponse(content={"is_valid": True})
+    #
+    # return JSONResponse(content={"is_valid": False})
