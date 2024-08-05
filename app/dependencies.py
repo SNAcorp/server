1c3610ff2c7326d6b1b5f21510aace8cf87625d6
SNA <snagithub@gmail.com>
2024-08-05 13:55:47 +0800

    rapeair access for order create

--- app/dependencies.py
+++ app/dependencies.py
@@ -39,8 +39,8 @@ async def get_current_user(request: Request, db: AsyncSession = Depends(get_db))
 
 
 async def get_admin_user(current_user: User = Depends(get_current_user)):
-    if not current_user.is_superuser or current_user.role == "admin":
-        raise HTTPException(status_code=403, detail=f"Not enough permissions for {current_user.name, current_user.role}")
+    if not current_user.is_superuser or current_user.role != "admin":
+        raise HTTPException(status_code=403, detail=f"Not enough permissions")
     return current_user
 
 
