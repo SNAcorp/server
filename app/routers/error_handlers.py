cbcefce346557cdd7d2ca17e0a9c83f6942b5477
SNA <snagithub@gmail.com>
2024-08-18 00:27:43 +0800

    repair middleware custom exception handler

--- app/routers/error_handlers.py
+++ app/routers/error_handlers.py
@@ -4,7 +4,6 @@ from fastapi.templating import Jinja2Templates
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from starlette.responses import HTMLResponse, RedirectResponse, Response
 
-
 templates = Jinja2Templates(directory="app/templates")
 
 
@@ -16,4 +15,5 @@ async def custom_401_handler(request: Request, exc: HTTPException):
     if request.url.path != "/login":
         return RedirectResponse("/login", status_code=303)
     else:
-        return Response(status_code=401)
+        return templates.TemplateResponse("login_register.html",
+                                          {"request": request})
